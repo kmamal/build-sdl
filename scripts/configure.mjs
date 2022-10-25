1608d7c2bafcdf8078c019871555fee9e1abe684
KM <the.real.laplace@gmail.com>
2022-10-25 17:20:14 +0300

    wip

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -12,7 +12,7 @@ let crossCompileFlag = ''
 if (platform === 'darwin') {
 	if (process.env.M1) {
 		crossCompileFlag = '--host=arm64-apple-darwin'
-		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch arm64 -target arm64-apple-darwin -mmacosx-version-min=11.0 -I/usr/local/include`.trim()
+		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch arm64 -mmacosx-version-min=11.0 -I/usr/local/include`.trim()
 		process.env.CLANG_LINK_ARM64 = `${process.env.LDFLAGS ?? ''} -mmacosx-version-min=11.0`.trim()
 	} else {
 		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`.trim()
