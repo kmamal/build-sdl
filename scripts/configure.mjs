2be22aa76dd504e3207848f16fcdd602a78dd6e3
KM <the.real.laplace@gmail.com>
2022-10-25 15:50:16 +0300

    wip

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -13,7 +13,7 @@ if (platform === 'darwin') {
 		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch arm64 -mmacosx-version-min=11.0 -I/usr/local/include`.trim()
 		process.env.CLANG_LINK_ARM64 = `${process.env.LDFLAGS ?? ''} -mmacosx-version-min=11.0`.trim()
 	} else {
-		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch ${arch} -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`.trim()
+		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch x86_64 -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`.trim()
 		process.env.LDFLAGS = `${process.env.LDFLAGS ?? ''} -mmacosx-version-min=10.9`.trim()
 	}
 }
