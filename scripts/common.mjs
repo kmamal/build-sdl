7276afda1501afd03aaef80848d1178268a48ca8
KM <the.real.laplace@gmail.com>
2022-10-25 03:13:57 +0300

    wip

--- scripts/common.mjs
+++ scripts/common.mjs
@@ -6,8 +6,7 @@ export const sdlDir = path.posix.resolve(rootDir, 'SDL')
 export const sdlBuildDir = path.posix.join(sdlDir, 'build')
 export const sdlOutDir = path.posix.join(sdlDir, 'out')
 
-const pkgFilePath = path.posix.join(rootDir, 'package.json')
-const pkg = await fs.readJson(pkgFilePath)
+const pkg = await fs.readJson('package.json')
 export const version = pkg.version.slice(0, pkg.version.indexOf('-'))
 export const [ , owner, repo ] = pkg.repository.url.match(/git@github.com:([^/]+)\/([^.]+).git/u)
 
