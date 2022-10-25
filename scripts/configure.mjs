9ac4b5c8d1e188ae256560f320026714dad9be54
KM <the.real.laplace@gmail.com>
2022-10-25 15:28:14 +0300

    wip

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -1,4 +1,4 @@
-import { posixSdlBuildDir, posixSdlOutDir, sysSdlBuildDir } from './common.mjs'
+import { platform, arch, posixSdlBuildDir, posixSdlOutDir, sysSdlBuildDir } from './common.mjs'
 
 await Promise.all([
 	$`mkdir -p ${posixSdlBuildDir}`,
@@ -7,4 +7,14 @@ await Promise.all([
 
 cd(sysSdlBuildDir)
 await $`make distclean || true`
+
+if (platform === 'darwin') {
+	if (process.env.M1) {
+		process.env.CFLAGS = `-arch ${arch} -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`
+		process.env.LDFLAGS = '-mmacosx-version-min=10.9'
+	} else {
+		process.env.CFLAGS = '-arch arm64 -mmacosx-version-min=11.0 -I/usr/local/include'
+		process.env.CLANG_LINK_ARM64 = '-mmacosx-version-min=11.0'
+	}
+}
 await $`../configure --prefix=${posixSdlOutDir}`
--- scripts/rebuild.mjs
+++ scripts/rebuild.mjs
@@ -5,6 +5,7 @@ await $`make -j$(nproc)`
 await $`make install`
 
 cd(sysSdlOutDir)
+await $`mv bin/* lib || true`
 await $`find . -type f ! -name '*.h' -and ! -name '*${sharedLibExt}*' -exec rm -f {} +`
 await $`mv include/SDL2/* include`
 await $`find . -depth -type d -empty -delete`
