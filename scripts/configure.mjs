a6947f5b3af9c0460e1537f28b24a33a44a5584e
KM <the.real.laplace@gmail.com>
2022-10-25 16:05:31 +0300

    wip

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -8,14 +8,16 @@ await Promise.all([
 cd(sysSdlBuildDir)
 await $`make distclean || true`
 
+let crossCompileFlags = ''
 if (platform === 'darwin') {
 	process.env.CC = 'clang'
 	if (process.env.M1) {
-		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch arm64 -mmacosx-version-min=11.0 -I/usr/local/include`.trim()
+		crossCompileFlags = '--host=arm-apple-darwin --build=aarch64-apple-darwin'
+		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -mmacosx-version-min=11.0 -I/usr/local/include`.trim()
 		process.env.CLANG_LINK_ARM64 = `${process.env.LDFLAGS ?? ''} -mmacosx-version-min=11.0`.trim()
 	} else {
-		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -arch x86_64 -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`.trim()
+		process.env.CFLAGS = `${process.env.CFLAGS ?? ''} -mmacosx-version-min=10.9 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I/usr/local/include`.trim()
 		process.env.LDFLAGS = `${process.env.LDFLAGS ?? ''} -mmacosx-version-min=10.9`.trim()
 	}
 }
-await $`../configure --prefix=${posixSdlOutDir}`
+await $`../configure ${crossCompileFlags} --prefix=${posixSdlOutDir}`
