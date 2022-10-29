fadfcc2593fb651e1600a9da3abb31e971b7d871
KM <the.real.laplace@gmail.com>
2022-10-29 03:49:06 +0300

    wip

--- scripts/util/common.js
+++ scripts/util/common.js
@@ -8,17 +8,6 @@ dir.build = Path.join(dir.root, 'build')
 dir.dist = Path.join(dir.root, 'dist')
 dir.publish = Path.join(dir.root, 'publish')
 
-const makePosixPath = (path) => path
-	.replace(/^([A-Z]):\\/u, (_, letter) => `/${letter.toLowerCase()}/`)
-	.replaceAll(/\\/ug, '/')
-
-dir.posix = {}
-dir.posix.root = makePosixPath(dir.root)
-dir.posix.src = makePosixPath(dir.src)
-dir.posix.build = makePosixPath(dir.build)
-dir.posix.dist = makePosixPath(dir.dist)
-dir.posix.publish = makePosixPath(dir.publish)
-
 const pkgPath = Path.join(dir.root, 'package.json')
 const pkg = JSON.parse(Fs.readFileSync(pkgPath).toString())
 const version = pkg.version.slice(0, pkg.version.indexOf('-'))
@@ -32,7 +21,6 @@ const assetName = `SDL-v${version}-${platform}-${targetArch}.tar.gz`
 
 module.exports = {
 	dir,
-	makePosixPath,
 	version,
 	owner,
 	repo,
