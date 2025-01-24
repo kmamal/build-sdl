33d2e6fabe72a3ae76f07eec1024e3d73b188794
KM <the.real.laplace@gmail.com>
2025-01-24 23:00:29 +0200

    fix scripts

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -11,9 +11,10 @@ let CFLAGS
 let LDFLAGS
 let crossCompileFlag
 if (C.platform === 'darwin') {
-	crossCompileFlag = process.env.CROSS_COMPILE_ARCH
-		? `-DCMAKE_OSX_ARCHITECTURES="${process.env.CROSS_COMPILE_ARCH}"`
-		: ''
+	let arch = process.env.CROSS_COMPILE_ARCH ?? C.arch
+	if (arch === 'x64') { arch = 'x86_64' }
+
+	crossCompileFlag = `-DCMAKE_OSX_ARCHITECTURES="${arch}"`
 
 	if (C.targetArch === 'arm64') {
 		CFLAGS = '-mmacosx-version-min=11.0'
